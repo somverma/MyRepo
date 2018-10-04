b417b770aafd4be57372bf066f442950726ec1ea
somverma <som.verma@capgemini.com>
2018-10-04 11:38:31 +0530

    FriendsManagement first commit

---
+++ FriendsManagement/src/main/java/com/friends/FriendsManagement/FriendsManagementApplication.java
@@ -0,0 +1,12 @@
+package com.friends.FriendsManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsManagementApplication.class, args);
+	}
+}
